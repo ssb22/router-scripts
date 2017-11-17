2390fe943a139ddc8cde5799db58fde553e719b3
Silas S. Brown <ssb22@cam.ac.uk>
2017-11-17 10:24:23 -0600

    Finally get this into version control

---
+++ build-sync.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget -N http://people.ds.cam.ac.uk/ssb22/setup/netgear.tgz http://people.ds.cam.ac.uk/ssb22/setup/upnp.tgz
+tar -zxf netgear.tgz ; tar -zxf upnp.tgz
+git add netgear-* upnp-*
+git commit -am update && git push
