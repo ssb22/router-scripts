09940515a94d8b58a94841cfa082268029f51b7e
Silas S. Brown <ssb22@hermes.cam.ac.uk>
2019-06-25 13:37:43 +0100

    update

--- git-sync.sh
+++ git-sync.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git pull --no-edit
 wget -N http://people.ds.cam.ac.uk/ssb22/setup/netgear.tgz http://people.ds.cam.ac.uk/ssb22/setup/upnp.tgz
 tar -zxf netgear.tgz ; tar -zxf upnp.tgz
 git add netgear-* upnp-*
