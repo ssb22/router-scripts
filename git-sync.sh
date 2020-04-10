2cb075f3d78c47cfb9ea2701b2a8a7bda9127708
Silas S. Brown <ssb22@cam.ac.uk>
2020-04-10 09:18:45 +0100

    Remove git-sync.sh (updates now done by homepage/Makefile itself)

--- git-sync.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git pull --no-edit
-wget -N http://ssb22.user.srcf.net/setup/netgear.tgz http://ssb22.user.srcf.net/setup/upnp.tgz
-tar -zxf netgear.tgz ; tar -zxf upnp.tgz
-git add netgear-* upnp-*
-git commit -am update && git push
